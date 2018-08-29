6aa610bff0d645961341c6dd48e52fcb45c86a38
Anoop P Oommen <allenshaji10@gmail.com>
2018-08-29 18:00:13 +0530

    improvements

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="/">Vueapp</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
